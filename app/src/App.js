333caf09255635c052c82f6113ff7f73c1099f39
santiago <sjorda@fi.uba.ar>
2021-02-24 02:56:28 -0300

    auth redirect

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,48 @@
 import ThemeProvider from "./components/theme/ThemeProvider";
 import Header from './components/header/Header'
+import { Route, Switch, useLocation } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 export default function App() {
   
+  let query = useQuery()
 
   return (
     <ThemeProvider>
       <Header />
+
+      <Switch>
+        <Route exact path="/auth">
+          { () => {
+            window.location.href = 'http://localhost:4000/spoty/auth'
+            return false
+            }
+          }
+
+        </Route>
+        <Route exact path="/callback">
+          <Callback code={query.get("code")} />
+        </Route>
+
+      </Switch>
+
     </ThemeProvider>
   );
 }
+
+
+function Callback(props) {
+  
+  const _code = props.code
+  
+  return (
+    <div>
+      the code is: {_code}
+    </div>
+  )
+}
+
 
--- sv/controllers/auth.controller.js
+++ sv/controllers/auth.controller.js
@@ -3,7 +3,7 @@ var request = require('request');
 const AUTH_API = 'https://accounts.spotify.com/authorize?',
       CLIENT_ID = '40d2fa449749415f9570c843dee768f6',
       CLIENT_SECRET = '26596c9d788a410d88dce1a3836bff19',
-      REDIRECT_URI = 'http://localhost:3000/spoty/callback',
+      REDIRECT_URI = 'http://localhost:3000/callback',
       TOKEN_URI = 'https://accounts.spotify.com/api/token',
       USER_URI = 'https://api.spotify.com/v1/me';
 
